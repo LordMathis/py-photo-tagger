fca935ad321cc8404542ad05bb4be836a318976d
LordMathis <matus@namesny.com>
2022-04-02 22:23:30 +0200

    Update main

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from watchdog.observers.inotify import InotifyObserver
 
 from tagger import DATA_BASE_PATH
 from tagger.dataset.dataset import DatasetHandler, DatasetWorker
-from tagger.db.mongo_client import init_db
 from tagger.model.model_register import ModelRegister
 from tagger.model.model_worker import ModelWorker
 from tagger.utils import logger
@@ -37,8 +36,6 @@ def main(watch: bool = False, ):
 
     model_register = ModelRegister()
     model_register.find_all_models()
-
-    init_db()
 
     threads = []
     input_queues: List[Queue] = []
